b4a7977b228ad8f9141946abfa828e6e51d85cc0
jerry.mei <jerry.mei@scrumcn.com>
2020-06-09 11:39:56 +0800

    完成用链表来实现两数相加的算法和测试用例

--- src/practice4/index.js
+++ src/practice4/index.js
@@ -24,8 +24,6 @@ export function array2List(arr) {
   return head.next;
 }
 
-
-
 /**
  * 两数相加
  * @param {ListNode} l1 
@@ -33,5 +31,30 @@ export function array2List(arr) {
  * @return {ListNode}
  */
 export function addTwoNumbers(l1, l2) {
-  return l2;
+  //声明一个head节点
+  const head = new ListNode(null);
+  //当前节点
+  let curr = head;
+  //进位
+  let carry = 0;
+  //当两个链表都不为空时，每位相加
+  while (l1 != null || l2 != null) {
+    //链表长度不一致，缺位补0
+    let x = l1 != null ? l1.val : 0;
+    let y = l2 != null ? l2.val : 0;
+    let sum = x + y + carry;
+    carry = Number.parseInt(sum / 10);
+    curr.next = new ListNode(sum % 10);
+    curr = curr.next;
+    if (l1 != null) {
+      l1 = l1.next;
+    }
+    if (l2 != null) {
+      l2 = l2.next;
+    }
+  }
+  if (carry > 0) {
+    curr.next = new ListNode(carry);
+  }
+  return head.next;
 }
--- src/practice4/index.test.js
+++ src/practice4/index.test.js
@@ -1,7 +1,63 @@
 import { addTwoNumbers, array2List } from './index'
 
 describe('用链表来实现两数相加', () => {
-  test('正常', () => {
-    array2List([1, 2, 3]);
+  test('两个链表都为空', () => {
+    const p1 = array2List([]);
+    const p2 = p1;
+    expect(addTwoNumbers(p1, p2)).toStrictEqual(null);
+  })
+
+  test('第一个链表为空，第二个不为空 -> 0 + 321 = 321', () => {
+    const p1 = array2List([]);
+    const p2 = array2List([1, 2, 3])
+    expect(addTwoNumbers(p1, p2)).toStrictEqual(p2);
+  })
+
+  test('第一个链表不为空，第二个为空 -> 321 + 0 = 321', () => {
+    const p1 = array2List([1, 2, 3]);
+    const p2 = array2List([])
+    expect(addTwoNumbers(p1, p2)).toStrictEqual(p1);
+  })
+
+  test('链表1的长度比链表2小，无进位 -> 10 + 321 = 331', () => {
+    const p1 = array2List([0, 1]);
+    const p2 = array2List([1, 2, 3])
+    expect(addTwoNumbers(p1, p2)).toStrictEqual(array2List([1, 3, 3]));
+  })
+
+  test('链表1的长度比链表2小，有进位 -> 99 + 321 = 420', () => {
+    const p1 = array2List([9, 9]);
+    const p2 = array2List([1, 2, 3])
+    expect(addTwoNumbers(p1, p2)).toStrictEqual(array2List([0, 2, 4]));
+  })
+
+  test('链表1的长度比链表2大，无进位 -> 321 + 10 = 331', () => {
+    const p1 = array2List([1, 2, 3])
+    const p2 = array2List([0, 1]);
+    expect(addTwoNumbers(p1, p2)).toStrictEqual(array2List([1, 3, 3]));
+  })
+
+  test('链表1的长度比链表2大，有进位 -> 321 + 99 = 420', () => {
+    const p1 = array2List([1, 2, 3])
+    const p2 = array2List([9, 9]);
+    expect(addTwoNumbers(p1, p2)).toStrictEqual(array2List([0, 2, 4]));
+  })
+
+  test('链表1的长度跟链表2一致，无进位 -> 120 + 321 = 531', () => {
+    const p1 = array2List([0, 1, 2])
+    const p2 = array2List([1, 2, 3]);
+    expect(addTwoNumbers(p1, p2)).toStrictEqual(array2List([1, 3, 5]));
+  })
+
+  test('链表1的长度跟链表2一致，有进位 -> 19 + 11 = 30', () => {
+    const p1 = array2List([9, 1])
+    const p2 = array2List([1, 1]);
+    expect(addTwoNumbers(p1, p2)).toStrictEqual(array2List([0, 3]));
+  })
+
+  test('链表1的长度跟链表2一致，有进位导致链表长度变长 -> 99 + 99 = 198', () => {
+    const p1 = array2List([9, 9])
+    const p2 = array2List([9, 9]);
+    expect(addTwoNumbers(p1, p2)).toStrictEqual(array2List([8, 9, 1]));
   })
 })
