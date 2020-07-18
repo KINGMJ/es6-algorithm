948aec13b19444b2bfebfe7f0276fb73429b66ca
KINGMJ <328047478@qq.com>
2020-07-19 00:14:57 +0800

    循环链表合并

--- src/practice8/index.js
+++ src/practice8/index.js
@@ -9,7 +9,40 @@ class Node {
 
 export default class CircleLinkList {
   constructor() {
-    this.head = new Node(null)  //头结点
+    this.rear = new Node(null)  //尾指针，空链表指向头结点
+    this.rear.next = this.rear  //带头结点的空链表，next指向自己
     this.length = 0             //链表长度
   }
+
+  /**
+   * 循环链表追加元素
+   * @param {} data 
+   */
+  append(data) {
+    const p = new Node(data)
+    //找到头结点
+    const head = this.rear.next
+    let current = head
+    //循环链表的循环判断条件，current.next 不等于头结点，则循环未结束
+    while (current.next !== head) {
+      current = current.next
+    }
+    current.next = p
+    p.next = head
+    this.rear = p
+    this.length++
+    return this
+  }
+}
+
+export function merge(listA, listB) {
+  //保存A表的头结点
+  const p = listA.rear.next
+  //将本是指向B表的第一个节点（不是头结点）赋值给 listA.rear.next
+  listA.rear.next = listB.rear.next.next
+  //将原A表的头结点赋值给listB.rear.next
+  listB.rear.next = p
+  //合并后rear为B的rear
+  listA.rear = listB.rear
+  return listA
 }
--- src/practice8/index.test.js
+++ src/practice8/index.test.js
@@ -1,2 +1,22 @@
-import { CircleLinkList } from './index'
+import CircleLinkList, { merge } from './index'
+
+describe('循环链表测试', () => {
+  test('测试', () => {
+    const circleLinkList1 = new CircleLinkList()
+
+    circleLinkList1.append(1)
+    circleLinkList1.append(2)
+    circleLinkList1.append(3)
+    console.log(circleLinkList1)
+
+
+    const circleLinkList2 = new CircleLinkList()
+    circleLinkList2.append("a")
+    circleLinkList2.append("b")
+    circleLinkList2.append("c")
+
+    console.log(merge(circleLinkList1, circleLinkList2))
+  })
+
+})
 
