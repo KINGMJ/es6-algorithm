2c8deda93a68fa5c34a168d3612a3d0dbc1ef5e7
KINGMJ <328047478@qq.com>
2020-08-19 23:09:56 +0800

    多叉树的前序遍历和层序遍历，递归实现

--- src/index.js
+++ src/index.js
@@ -1,50 +1,64 @@
-function arr2Tree(arr) {
-  if (arr.length == 0) {
-    return arr
-  }
-  const head = arr[0]
-  const tree = arr2Tree(arr.slice(1))
-  const childrens = [], siblings = [];
-  for (let i = 0; i < tree.length; i++) {
-    if (head.id == tree[i].parent_id) {
-      childrens.push(tree[i])
-    } else {
-      siblings.push(tree[i])
+const tree = {
+  name: 'A', childeren: [
+    {
+      name: 'B', childeren: [
+        {
+          name: 'B1', childeren: [
+            { name: 'B11' },
+            { name: 'B12' },
+            { name: 'B13' }
+          ]
+        },
+        {
+          name: 'B2'
+        }]
+    },
+    {
+      name: 'C',
+      childeren: [
+        { name: 'C1' },
+        { name: 'C2' }
+      ]
+    },
+    {
+      name: 'D',
+      childeren: [{ name: 'D1' }]
     }
-  }
-  if (childrens.length > 0) {
-    head.children = childrens
-  }
-  return [head].concat(siblings)
+  ]
 }
 
-function arr2Tree2(arr, pid) {
-  let res = [];
-  arr.forEach(item => {
-    if (item.parent_id === pid) {
-      const result = arr2Tree2(arr, item.id);
-      if (result.length) {
-        item.children = result;
-      }
-      res.push(item);
-    }
-  });
-  return res;
+
+// console.log(tree)
+// console.log(preOrderTraverse(tree))
+console.log(levelOrderTraverse([tree]))
+
+
+//树的前序遍历
+//二叉树是先遍历根，再遍历左子树，再遍历右子树。对于多叉树，原理相同，区别就是遍历左子树和右子树变成循环对子树进行递归遍历算法
+function preOrderTraverse(root) {
+  console.log(root)
+  if (root.childeren == null) {
+    return
+  }
+  for (let item of root.childeren) {
+    preOrderTraverse(item)
+  }
 }
 
-const folders = [
-  { id: 1, name: '全部文件', parent_id: null },
-  { id: 2, name: '图片', parent_id: 1 },
-  { id: 3, name: '文档', parent_id: 1 },
-  { id: 4, name: '视频', parent_id: 1 },
-  { id: 5, name: '壁纸', parent_id: 2 },
-  { id: 6, name: '头像', parent_id: 2 },
-  { id: 7, name: '电影', parent_id: 4 },
-  { id: 8, name: '纪录片', parent_id: 4 },
-  { id: 9, name: '风景', parent_id: 5 },
-  { id: 10, name: '动漫', parent_id: 5 },
-  { id: 11, name: '游戏', parent_id: 5 }
-]
+//树的前序遍历和层序遍历对应到图里面就是深度优先和广度优先搜索
 
-//console.log(arr2Tree(folders))
-console.log(arr2Tree2(folders, null))
+
+//树的层序遍历
+function levelOrderTraverse(root) {
+  if (root.length == 0) {
+    return;
+  }
+  let temp = []
+  for (let item of root) {
+    console.log(item.name)
+    if (item.childeren != null) {
+      temp = temp.concat(item.childeren)
+    }
+  }
+  levelOrderTraverse(temp)
+}
