2bc402e420ed0fb29b04048b98c29e91fa8c8ca2
KINGMJ <328047478@qq.com>
2021-02-23 21:55:08 +0800

    冒泡排序

--- src/practice18/index2.js
+++ src/practice18/index2.js
@@ -1,3 +1,13 @@
 export default function bubbleSort(arr) {
+  let temp;
+  for (let i = 0; i < arr.length - 1; i++) {
+    for (let j = i + 1; j < arr.length; j++) {
+      if (arr[i] > arr[j]) {
+        temp = arr[i];
+        arr[i] = arr[j];
+        arr[j] = temp;
+      }
+    }
+  }
   return arr;
 }
