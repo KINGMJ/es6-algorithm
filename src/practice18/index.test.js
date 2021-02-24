9c4d15ca970d13ba2c1ba13aaec4ed4a42d4e256
KINGMJ <328047478@qq.com>
2021-02-24 21:30:41 +0800

    插入排序

--- src/practice18/index.test.js
+++ src/practice18/index.test.js
@@ -1,6 +1,7 @@
 import selectionSort from "./index";
 import quickSort from "./index1";
 import bubbleSort from "./index2";
+import insertionSort from "./index3";
 
 describe("排序算法测试", () => {
   test("选择排序，从小到大排列", () => {
@@ -29,5 +30,13 @@ describe("冒泡排序测试", () => {
     let arr = [9, 1, 5, 8, 3, 7, 4, 6, 2];
     arr = bubbleSort(arr);
     expect(arr).toStrictEqual([1, 2, 3, 4, 5, 6, 7, 8, 9]);
+  });
+});
+
+describe("插入排序测试", () => {
+  test("[7,6,9,3,1,5,2,4]", () => {
+    let arr = [7, 6, 9, 3, 1, 5, 2, 4];
+    arr = insertionSort(arr);
+    expect(arr).toStrictEqual([1, 2, 3, 4, 5, 6, 7, 9]);
   });
 });
---
+++ src/practice18/index3.js
@@ -0,0 +1,11 @@
+export default function insertionSort(arr) {
+  let temp, inner;
+  for (let outer = 1; outer < arr.length; outer++) {
+    temp = arr[outer];
+    for (inner = outer - 1; inner >= 0 && arr[inner] > temp; inner--) {
+      arr[inner + 1] = arr[inner];
+    }
+    arr[inner + 1] = temp;
+  }
+  return arr;
+}
