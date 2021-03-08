b37926ff3708c022280d658338ad3e747e33f6d7
KINGMJ <328047478@qq.com>
2021-03-08 21:06:57 +0800

    增加测试用例

--- src/practice23/index.test.js
+++ src/practice23/index.test.js
@@ -7,9 +7,23 @@ describe("二叉树的中序遍历 -> 递归解法", () => {
     expect(inorderTraversal(tree)).toStrictEqual([1, 3, 2]);
   });
 
-  test(" []", () => {
+  test("[]", () => {
     const tree = arrayToTree([]);
-    console.log(tree);
     expect(inorderTraversal(tree)).toStrictEqual([]);
+  });
+
+  test("[1]", () => {
+    const tree = arrayToTree([1]);
+    expect(inorderTraversal(tree)).toStrictEqual([1]);
+  });
+
+  test("[1,2]", () => {
+    const tree = arrayToTree([1, 2]);
+    expect(inorderTraversal(tree)).toStrictEqual([2, 1]);
+  });
+
+  test("[1,null,2]", () => {
+    const tree = arrayToTree([1, null, 2]);
+    expect(inorderTraversal(tree)).toStrictEqual([1, 2]);
   });
 });
