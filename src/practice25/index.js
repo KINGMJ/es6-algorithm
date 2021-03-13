c0d51445fab4b4297299c1f62bb835d5ada7fd6b
KINGMJ <328047478@qq.com>
2021-03-13 16:49:20 +0800

    二叉树的后续遍历，递归写法

--- src/practice25/index.js
+++ src/practice25/index.js
@@ -1,1 +1,14 @@
-export function postorderTraversal(root) {}
+export function postorderTraversal(root) {
+  let arr = [];
+  traversal(root, arr);
+  return arr;
+
+  function traversal(root, arr) {
+    if (root == null) {
+      return;
+    }
+    traversal(root.left, arr);
+    traversal(root.right, arr);
+    arr.push(root.val);
+  }
+}
--- src/practice25/index.test.js
+++ src/practice25/index.test.js
@@ -1,4 +1,29 @@
 import { arrayToTree } from "../structure/binaryTree";
 import { postorderTraversal } from "./index";
 
-describe("二叉树的后续遍历", () => {});
+describe("二叉树的后续遍历", () => {
+  test("[1, null,2,3]", () => {
+    const tree = arrayToTree([1, null, 2, 3]);
+    expect(postorderTraversal(tree)).toStrictEqual([3, 2, 1]);
+  });
+
+  test("[]", () => {
+    const tree = arrayToTree([]);
+    expect(postorderTraversal(tree)).toStrictEqual([]);
+  });
+
+  test("[1]", () => {
+    const tree = arrayToTree([1]);
+    expect(postorderTraversal(tree)).toStrictEqual([1]);
+  });
+
+  test("[1,2]", () => {
+    const tree = arrayToTree([1, 2]);
+    expect(postorderTraversal(tree)).toStrictEqual([2, 1]);
+  });
+
+  test("[1,null,2]", () => {
+    const tree = arrayToTree([1, null, 2]);
+    expect(postorderTraversal(tree)).toStrictEqual([2, 1]);
+  });
+});
